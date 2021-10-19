b89afeb2104d04ed2874f645c4e2d0bd2369a793
Carlos Araujo <56395845+caarlosp23@users.noreply.github.com>
2021-10-18 21:51:21 -0300

    Add project files.

---
+++ mediaTeste/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace mediaTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, media;
+            Console.Write("Digite o primeiro valor: ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo valor: ");
+            b = double.Parse(Console.ReadLine());
+            media = (a + b) / 2;
+            Console.WriteLine("Média: " + media);
+        }
+    }
+}
